072cb28aa46a8eaaef7a56dc6ff898f1072036f6
Curtis McEnroe <programble@gmail.com>
2015-06-15 14:09:43 -0400

    Refactor and document itoa

--- src/itoa.asm
+++ src/itoa.asm
@@ -2,24 +2,24 @@ section .data
 
 digits db '0123456789ABCDEF'
 
-section .bss
-
-fstr resb 34
+; Maximum length, 32 bits formatted as binary and a null-terminator.
+output db '00000000000000000000000000000000', 0
 
 section .text
 
 ; itoa(dword number, byte width, byte radix)
+; Format number as string of width in radix. Returns pointer to string.
 global itoa
 itoa:
   push ebp
   mov ebp, esp
   push ebx
+  push esi
   push edi
 
-  ; Start at end of output string, set null terminator.
-  mov edi, fstr
-  add edi, 33
-  mov byte [edi], 0
+  ; Start at end of output string and work backwards.
+  lea edi, [output + 32]
+  std
 
   ; Keep track of width.
   xor cl, cl
@@ -28,25 +28,26 @@ itoa:
   mov eax, [ebp + 8] ; number
 
   .loop:
-    dec edi
-
     ; Divide number by radix.
     movzx ebx, byte [ebp + 13] ; radix
     cdq
     div ebx
 
     ; Use remainder to set digit in output string.
-    mov edx, [digits + edx]
-    mov [edi], dl
+    lea esi, [digits + edx]
+    movsb
 
     ; Check if we've reached the right width yet.
     inc cl
     cmp cl, [ebp + 12] ; width
     jl .loop
 
-  mov eax, edi
+  ; The last movsb brought us too far back.
+  lea eax, [edi + 1]
 
+  cld
   pop edi
+  pop esi
   pop ebx
   mov esp, ebp
   pop ebp
