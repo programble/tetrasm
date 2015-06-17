8bae0d8420835f15ac40368cc18d9d83b4ad0ed1
Curtis McEnroe <programble@gmail.com>
2015-06-17 12:42:52 -0400

    Move text mode cursor off screen

--- src/main.asm
+++ src/main.asm
@@ -4,6 +4,14 @@ extern tests
 
 global main
 main:
+  ; Move text mode cursor off screen.
+  mov dx, 0x3D4
+  mov al, 0x0E
+  out dx, al
+  inc dx
+  mov al, 0xFF
+  out dx, al
+
 %ifdef TEST
   jmp tests
 %endif
