0126ffe465b9f0075741324207d99241682cce9d
Curtis McEnroe <programble@gmail.com>
2015-06-28 17:50:10 -0400

    Add keybindings to game loop

--- src/game.asm
+++ src/game.asm
@@ -1,4 +1,5 @@
 %include "video.mac"
+%include "keyboard.mac"
 
 section .text
 
@@ -7,7 +8,9 @@ extern about_draw
 extern calibrate
 extern bag_init
 extern well_draw
-extern current_spawn, current_draw
+extern current_spawn, current_left, current_right, current_down, current_draw
+extern scan
+extern reset
 
 global game
 game:
@@ -28,8 +31,39 @@ game:
   call bag_init
   call current_spawn
 
-  call well_draw
-  call current_draw
+  .loop:
+    call scan
+    push ax
+    .left:
+      cmp byte [esp], KEY_LEFT
+      jne .right
+      call current_left
+      inc ebx
+    .right:
+      cmp byte [esp], KEY_RIGHT
+      jne .down
+      call current_right
+      inc ebx
+    .down:
+      cmp byte [esp], KEY_DOWN
+      jne .reset
+      call current_down
+      inc ebx
+    .reset:
+      cmp byte [esp], KEY_R
+      jne .end
+      jmp reset
 
-  hlt
-  jmp game
+    .end:
+      add esp, 2
+
+    .draw:
+      test ebx, ebx
+      jz .continue
+
+      call well_draw
+      call current_draw
+
+    .continue:
+      xor ebx, ebx
+      jmp .loop
