fe3122a304b027a4fcd5c99f1cafc2baa31c6b29
Curtis McEnroe <programble@gmail.com>
2015-06-28 17:49:42 -0400

    Implement current tetromino movement functions

--- src/current.asm
+++ src/current.asm
@@ -14,6 +14,7 @@ section .text
 
 extern tetrominoes
 extern bag_pop
+extern well_collide
 extern draw
 
 ; current_spawn()
@@ -24,6 +25,70 @@ current_spawn:
   mov [current_offset], ax
   mov word [current_coords], WELL_WIDTH / 2 - 4
   ret
+
+; current_left()
+; Move the current tetromino left, if possible. Returns result of well_collide.
+global current_left
+current_left:
+  ; Check if x == 0.
+  push word [current_coords]
+  cmp byte [esp], 0
+  jne .check
+
+  ; Return 0.
+  xor eax, eax
+  add esp, 2
+  ret
+
+  .check:
+    ; Check for collisions to the left.
+    dec byte [esp]
+    push word [current_offset]
+    call well_collide
+    jz .ret
+
+    ; Update actual coordinates.
+    dec byte [current_coords]
+
+  .ret:
+    add esp, 4
+    ret
+
+; current_right()
+; Move the current tetromino right, if possible. Returns result of well_collide.
+global current_right
+current_right:
+  ; Check for collisions to the right.
+  push word [current_coords]
+  inc byte [esp]
+  push word [current_offset]
+  call well_collide
+  jz .ret
+
+  ; Update actual coordinates.
+  inc byte [current_coords]
+
+  .ret:
+    add esp, 4
+    ret
+
+; current_down()
+; Move the current tetromino down, if possible. Returns result of well_collide.
+global current_down
+current_down:
+  ; Check for collisions one row down.
+  push word [current_coords]
+  inc byte [esp + 1]
+  push word [current_offset]
+  call well_collide
+  jz .ret
+
+  ; Update actual coordinates.
+  inc byte [current_coords + 1]
+
+  .ret:
+    add esp, 4
+    ret
 
 ; current_draw()
 ; Draw the current tetromino inside the well.
